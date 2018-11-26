f6a953a8ced294ea10c4fd0c34749a301286fc6a
protpolymer <nvsvkont@gmail.com>
2018-11-26 19:26:13 +0300

    added junit tests

--- src/test/java/ru/itpark/CreditCalculatorServiceTest.java
+++ src/test/java/ru/itpark/CreditCalculatorServiceTest.java
@@ -9,7 +9,20 @@ class CreditCalculatorServiceTest {
     @Test
     void amountCredit() {
         {
-
+            CreditCalculatorService creditCalculatorServiceTest = new CreditCalculatorService();
+            String test = creditCalculatorServiceTest.amountCredit(1_000, 18.5, 60);
+            assertEquals(test, "1539.97");
         }
+        {
+            CreditCalculatorService creditCalculatorServiceTest = new CreditCalculatorService();
+            String test = creditCalculatorServiceTest.amountCredit(10_000_000, 50, 100);
+            assertEquals(test, "42381658.79");
+        }
+        {
+            CreditCalculatorService creditCalculatorServiceTest = new CreditCalculatorService();
+            String test = creditCalculatorServiceTest.amountCredit(2_354_987, 13.2, 24);
+            assertEquals(test, "2692361.58");
+        }
+
     }
 }
